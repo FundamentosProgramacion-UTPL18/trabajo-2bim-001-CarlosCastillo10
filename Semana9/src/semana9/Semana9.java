e7f61cc6869eb4b46830cd9d4c655db483e01cc7
Carlos Castillo <carloscastillo090916@gmail.com>
2018-05-31 10:05:58 -0500

    Agregando trabajo Semana9

---
+++ Semana9/src/semana9/Semana9.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana9;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Semana9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+            int arreglo[] = new int [4];
+            arreglo[0] = 10;
+
+            for (int contador = 0; contador < arreglo.length; contador ++){
+                       System.out.println(arreglo[contador]);
+                       
+            }
+        }
+
+    }
